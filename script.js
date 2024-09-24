5a81754fa89b588ca7d05e81eeb7be8d7718bfa1
Johanna Eriksson <johanna.eriksson@schibsted.com>
2024-09-24 13:29:34 +0200

    Add HTML file and JS file. Fetch data from space API and display the number of people in space along with their names and spacecraft

---
+++ script.js
@@ -0,0 +1,14 @@
+const container = document.getElementById('astros')
+
+fetch('http://api.open-notify.org/astros.json')
+    .then((response) => {
+        return response.json()
+    })
+    .then((json) => {
+        container.innerHTML = `<h1>There are ${json.number} people in space right now</h1>`
+
+        json.people.forEach((person) => {
+            container.innerHTML += `<p>${person.name} is on the ${person.craft}</p>`
+
+        })
+    })
