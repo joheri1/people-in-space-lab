551578451b7a867341cfea882cb6f0c5899b187c
Johanna Eriksson <johanna.eriksson@schibsted.com>
2024-09-24 13:47:54 +0200

    Add Error handling in JS code

--- script.js
+++ script.js
@@ -2,13 +2,22 @@ const container = document.getElementById('astros')
 
 fetch('http://api.open-notify.org/astros.json')
     .then((response) => {
+        if (!response.ok) {
+            throw new Error(`HTTP Error! Status ${response.status}`)
+        }
         return response.json()
     })
-    .then((json) => {
-        container.innerHTML = `<h1>There are ${json.number} people in space right now</h1>`
+    .then((data) => {
+        console.log(data)
 
-        json.people.forEach((person) => {
+        // Update the DOM with the information from the API
+        container.innerHTML = `<h1>There are ${data.number} people in space right now</h1>`
+
+        data.people.forEach((person) => {
             container.innerHTML += `<p>${person.name} is on the ${person.craft}</p>`
-
         })
+    })
+    .catch((error) => {
+        console.log('Fetch error:', error)
+        container.innerHTML = `<p>Ett fel uppstod: ${error.message}</p>`
     })
